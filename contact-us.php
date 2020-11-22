02ce3789ae03c68d2f5d1fb8664b35b8c71152ad
Gloria Curran <glocurran7@gmail.com>
2020-11-21 19:22:27 -0500

    initial file commit

---
+++ contact-us.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+	echo ('<h2>hello world</h2>');
+	phpinfo();
+	?>
+</body>
+</html>
